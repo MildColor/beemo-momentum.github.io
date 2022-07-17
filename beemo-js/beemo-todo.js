e98cc005faa657dc06da8cdb0101214bdabf3dd0
MildColor <96479626+MildColor@users.noreply.github.com>
2022-07-18 04:54:57 +0900

    init

---
+++ beemo-js/beemo-todo.js
@@ -0,0 +1,80 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.querySelector("#todo-list");
+const TODOS_KEY = "todos";
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+function deleteToDo(event) {
+  const btn = event.target.parentElement;
+  const li = btn.parentElement;
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+  li.remove();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const marker = document.createElement("img");
+  marker.className = "marker";
+  marker.src = "img/12.png";
+  li.appendChild(marker);
+
+  const span = document.createElement("span");
+
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  // button.innerText = "❌";
+  const img = document.createElement("img");
+  img.className = "rdpic";
+  const images = [
+    "0",
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+  ];
+  const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+  img.src = `img/${chosenImage}.png`;
+  button.appendChild(img);
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDoInput.value = "";
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
+
+function sexyFilter() {}
